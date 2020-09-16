72cb0b7301e583f2dae139e0de87f49afc710892
Karan Shah <karanshah0206@outlook.com>
2020-09-17 04:38:56 +0530

    Join Channel And Create Local Stream

--- scripts/script.js
+++ scripts/script.js
@@ -20,3 +20,14 @@ function removeVideoStream (elementId) {
 // Create client by invoking the AgoraRTC create client method. Takes JSON to initialize basic settings.
 let client  = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 client.init("c0041179099d492fa2dafcc82ec735c0"); // This function requires the App ID in string
+
+// Join a specific channel (room).
+client.join(null /* token - null if not present */, "channel-name", null /* if you need to specify custom ID */, (uid) => {
+
+    // Create stream for local user.
+    let localStream = AgoraRTC.createStream({ video: true, audio: true });
+    localStream.init(() => {
+        localStream.play("me"); // Play local stream "#me" div
+        client.publish(localStream, handleFail); // Publish stream on server
+    }, handleFail);
+}, handleFail);
